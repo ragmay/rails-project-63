7052747b4d8dc50d56704b34acc6c519dd6a916e
Alexandr Pameev <5567960@gmail.com>
2022-10-11 00:47:11 +0300

    generate gem and makefile

---
+++ lib/forms_generator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "forms_generator/version"
+
+module FormsGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/forms_generator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FormsGenerator
+  VERSION = "0.1.0"
+end
