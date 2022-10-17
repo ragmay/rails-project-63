6d8fdea6c26d22d6117930fd3f74ff8919f79ea1
Alexandr Pameev <5567960@gmail.com>
2022-10-18 00:01:09 +0300

    generation of fields

--- lib/forms_generator.rb
+++ lib/forms_generator.rb
@@ -7,21 +7,56 @@ module FormsGenerator
   class Error < StandardError; end
   autoload "Tag", "forms_generator/tag"
 
-  def self.form_for(_name_user, url = {})
-    url[:url] = "#" unless url[:url]
-    "<form action=\"#{url[:url]}\" method=\"post\"></form>"
+  def self.form_for(user_structure, url = {})
+    @user_structure = user_structure
+    @field_generation = ""
+    yield FormsGenerator if block_given?
+    Tag.build("form", action: url[:url] || "#", method: url[:method] || "post") { @field_generation }
   end
 
-  def self.input(user); end
+  def self.input(field, **attributes_field)
+    attributes_field[:name] = field
+    if attributes_field[:as] == :text
+      attributes_field.delete(:as)
+      @field_generation += Tag.build("textarea", name: field, cols: attributes_field[:cols] || "20",
+                                                 rows: attributes_field[:rows] || "40", **attributes_field) do
+        @user_structure.public_send(field)
+      end
+    else
+      @field_generation += Tag.build("input", **attributes_field, type: "text",
+                                                                  value: @user_structure.public_send(field))
+    end
+  end
 
   User = Struct.new(:name, :job, :gender, keyword_init: true)
   user = User.new name: "rob", job: "hexlet", gender: "m"
 
-  form =
-    FormsGenerator.form_for user, url: "/users" do |f|
-      f.input
-      f.input :job, as: :text
-    end
+  FormsGenerator.form_for user, url: "/users" do |f|
+    f.input :name
+    f.input :job, as: :text
+  end
 
-  puts form
+  FormsGenerator.form_for user do |f|
+    f.input :name
+    f.input :job, as: :text
+  end
+
+  FormsGenerator.form_for user, url: "#" do |f|
+    f.input :name, class: "user-input"
+    f.input :job
+  end
+
+  FormsGenerator.form_for user do |f|
+    f.input :job, as: :text
+  end
+
+  FormsGenerator.form_for user, url: "#" do |f|
+    f.input :job, as: :text, rows: 50, cols: 50
+  end
+
+  FormsGenerator.form_for user, url: "/users" do |f|
+    f.input :name
+    f.input :job, as: :text
+    f.input :age
+  end
 end
